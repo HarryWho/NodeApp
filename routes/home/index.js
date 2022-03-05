f15fad51c681f5f96f89ce989dbf959cd23bcd8b
HarryWho <calvin.bartlett42@gmail.com>
2022-03-05 19:02:33 +1100

    Google Auth done

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router();
 
+// @desc: Login/Landing page
+// @route GET /
 router.get('/', (req, res) => {
   res.render("home/home")
 })
 
+// @desc: dashboard page
+// @route GET /dashboard
+router.get('/dashboard', (req, res) => {
+  res.render("home/dashboard")
+})
 
+// @desc: login page
+// @route GET /login
+router.get('/login', (req, res) => {
+  res.render("home/login", { layout: 'layouts/login' })
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,40 +1,61 @@
-require('dotenv').config()
-require('ejs');
+const morgan = require('morgan')
+const dotenv = require('dotenv')
 const express = require('express')
-const mongoose = require('mongoose')
 const expressLayouts = require('express-ejs-layouts')
+const ConnectDB = require('./config/DB');
+const passport = require('passport')
+const session = require('express-session')
+require('ejs');
 
+
+
+// load config
+dotenv.config({ path: './config/config.env' })
+
+// passport config
+require('./config/passport')(passport)
 
 // create the app server
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+
+  app.use(morgan('dev'))
+}
+
 // static public folder
 app.use(express.static('public'))
 
-// ejs and express layouts set up
+// ejs and express layout s set up
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('layout', 'layouts/layout')
 
+// Express session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Routes
 const homeRoute = require('./routes/home/index')
 const userRoute = require('./routes/user/user-route')
-
+const authRoute = require('./routes/auth/auth')
 app.use('/', homeRoute);
 app.use('/user', userRoute);
+app.use('/auth', authRoute);
 
-
-// MongoDB Connect and monitor
-mongoose.connect(process.env.MONGO_URL)
-const db = mongoose.connection;
-db.on('error', (error) => {
-  console.log(error.message)
-})
-db.once('open', () => {
-  console.log("Mongo now connected")
-})
+// connect MongoDB
+ConnectDB()
 
 // Start Server
-app.listen(process.env.PORT, () => {
-  console.log(`Server runing on port ${process.env.PORT}`)
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
 })
