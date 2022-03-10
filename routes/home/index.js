44528bcfcbc5f954ac6b95157995e8218990b5c7
HarryWho <calvin.bartlett42@gmail.com>
2022-03-10 11:22:38 +1100

    Initial commit

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,58 +1,9 @@
 const express = require('express')
 const router = express.Router();
-const { ensureAuth, ensureGuest } = require('../../middleware/auth')
-const Story = require('../../models/Story')
 
-
-// @desc: Login/Landing page
-// @route GET /
 router.get('/', (req, res) => {
-  res.render("home/home", { user: req.user })
+  res.render("home/home")
 })
 
-// @desc: All Public Stories
-// @route GET /
-router.get('/public', ensureAuth, async(req, res) => {
-  try {
-    const stories = await Story.find({ status: 'public' })
-      .populate({ path: 'user' })
-      .sort({ createdAt: 'desc' })
-      .lean();
 
-    res.render('stories/stories', {
-      user: req.user,
-      stories: stories,
-      caption: 'By All Authors',
-      helper: require('../../helpers/helpers')
-    })
-  } catch (err) {
-    console.log(err)
-    res.render('error/500', { user: req.user })
-  }
-
-})
-
-// @desc: dashboard page
-// @route GET /dashboard
-router.get('/dashboard', ensureAuth, async(req, res) => {
-  try {
-    const stories = await Story.find({ user: req.user.id }).lean()
-    res.render("home/dashboard", {
-      user: req.user,
-      stories: stories,
-      helper: require('../../helpers/helpers')
-
-    })
-  } catch (err) {
-    console.log(err)
-    res.render('error/500')
-  }
-})
-
-// @desc: login page
-// @route GET /login
-router.get('/login', ensureGuest, (req, res) => {
-
-  res.render("home/login", { layout: 'layouts/login' })
-})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,117 +1,39 @@
-const morgan = require('morgan')
-const dotenv = require('dotenv')
+require('dotenv').config()
+require('ejs');
 const express = require('express')
 const mongoose = require('mongoose')
 const expressLayouts = require('express-ejs-layouts')
-const ConnectDB = require('./config/DB');
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')
-const methodOverride = require('method-override')
-const { google, facebook } = require('./config/passport')
-const WebSocket = require('ws')
-require('ejs');
 
-
-
-// load config
-dotenv.config({ path: './config/config.env' })
-
-// passport config
-const myGooglePassport = google(passport)
-
-const myFaceBookPassport = facebook(passport)
 
 // create the app server
 const app = express()
-const server = require('http').createServer(app)
-const wss = new WebSocket.Server({ server: server })
-
-// Body parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-
-// Logging
-if (process.env.NODE_ENV === 'development') {
-
-  app.use(morgan('dev'))
-}
 
 // static public folder
 app.use(express.static('public'))
 
-// EJS Helpers
-const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/helpers')
-
-app.use((req, res, next) => {
-  res.locals.formatDate = formatDate,
-    res.locals.truncate = truncate,
-    res.locals.stripTags = stripTags,
-    res.locals.editIcon = editIcon,
-    res.locals.select = select,
-    next()
-})
-
-// ejs and express layout s set up
+// ejs and express layouts set up
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('layout', 'layouts/layout')
-app.set("layout extractStyles", true)
-app.set('layout extractScripts', true)
-
-// Express session
-app.use(session({
-  secret: 'keyboard cat',
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI,
-    mongooseConnection: mongoose.connection
-  }),
-  resave: false,
-  saveUninitialized: false
-}))
-
-// Passport Middleware
-app.use(passport.initialize())
-app.use(passport.session())
-
-// Method Override
-app.use(methodOverride('_method'))
-
 
 // Routes
 const homeRoute = require('./routes/home/index')
-const userRoute = require('./routes/user/user-route')
-const authRoute = require('./routes/auth/auth')
-const storyRoute = require('./routes/stories/index');
+
 app.use('/', homeRoute);
-app.use('/user', userRoute);
-app.use('/auth', authRoute);
-app.use('/stories', storyRoute);
-
-// connect MongoDB
-ConnectDB()
-
-// Websocket Connection Code
-wss.on('connection', function connection(ws, req) {
-
-  ws.send('Welcome new client');
-
-  ws.on('message', function message(data, isBinary) {
-    console.log('received: %s', data);
-
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
-  });
 
 
-});
 
+// MongoDB Connect and monitor
+mongoose.connect(process.env.MONGO_URL)
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.log(error.message)
+})
+db.once('open', () => {
+  console.log("Mongo now connected")
+})
 
 // Start Server
-const PORT = process.env.PORT || 4000
-server.listen(PORT, () => {
-  console.log(`Server runing in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Server runing on port ${process.env.PORT}`)
 })
